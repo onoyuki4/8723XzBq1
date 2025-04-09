1e880d21bb0bf50e7b6b5017b86b5d687b4d2bfe
onoyuki4 <yukihgpo@gmail.com>
2025-04-10 06:11:50 +0900

    Update config.js

--- works/work01/config.js
+++ works/work01/config.js
@@ -1,10 +1,13 @@
 // works/work01/config.js
 
-// 이 작품의 이미지 파일 목록 (순서대로)
-const workImages = [
-    '1.jpg',    // 이 파일이 메인 페이지 썸네일로 사용됨
+const workConfig = {
+  title: "paofan pixiv",       // 작품 제목
+  author: "paofan",          // 작가 이름
+  images: [                   // 이미지 파일 목록
+    '1.jpg',
     '2.jpg',
     '3.webp',
     '4.webp',
     '5.webp'
-];
+  ]
+};
